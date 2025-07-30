3f0efbe70d88ea0d9a47fcfad7ee512de30f0ffb
espindolas95 <espindolasol.ok@gmail.com>
2025-07-30 15:38:54 +0200

    feat(definizione ed esportazione della funzione file names)

---
+++ names.js
@@ -0,0 +1,11 @@
+//definizione della funzione
+const getFullName = (firstName,lastName) => {
+    return { 
+        firstName,
+        lastName   
+    }
+}
+    
+   
+//esporto la funzione user
+module.exports = getFullName; 
