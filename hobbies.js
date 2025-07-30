5a68a49d20f55cd1ff7b8a41429134df1a87bebc
espindolas95 <espindolasol.ok@gmail.com>
2025-07-30 15:50:18 +0200

    feat(funzione con i tre parametri hobby ed esportazione della cartella hobbies)

---
+++ hobbies.js
@@ -0,0 +1,11 @@
+ // definizione della funzione che accetta 3 parametri
+ const gethobbies = {hobbiOne, hobbiTwo, hobbyThree} => {
+    return[
+        hobbyOne,
+        hobbyTwo,
+        hobbyThree
+    ]
+
+ }
+
+ module.exports = gethobbies;
